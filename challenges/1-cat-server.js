c8f9cb979f2b6a12a6c52256cb6a743b8cf4b2ae
Jamie Radcliffe <jamie_michael@hotmail.co.uk>
2023-01-31 17:06:30 +0000

    completed to fetchCatPics

--- challenges/1-cat-server.js
+++ challenges/1-cat-server.js
@@ -4,7 +4,6 @@ const request = require('../utils/server');
 
 //your function should invoke the request function with the path /status and the callback function
 
-
 function checkServerStatus(callback) {
   request('/status', callback);
 }
@@ -19,17 +18,43 @@ function checkServerStatus(callback) {
 
 function fetchBannerContent(bannerCallback) {
   request('/banner', (err, data) => {
-    const newBannerData = {...data}
+    const newBannerData = { ...data };
     newBannerData.copyrightYear = 2023;
     bannerCallback(err, newBannerData);
-  })
+  });
 }
 
-function fetchAllOwners() {}
+function fetchAllOwners(ownersCallback) {
+  request('/owners', (err, owners) => {
+    let newArr = [];
+    for (let owner of owners) {
+      newArr.push(owner.toLowerCase());
+    }
+    console.log(owners);
+    ownersCallback(err, newArr);
+  });
+}
 
-function fetchCatsByOwner() {}
-
-function fetchCatPics() {}
+function fetchCatsByOwner(ownerName, callbackFunc) {
+  request(`/owners/${ownerName}/cats`, (err, data) => {
+    callbackFunc(err, data);
+  });
+}
+// this function should take an array of strings (names of cat pics) and a callback function
+// for each cat name in the passed array, a request should be sent to /pics/:catpic
+// each response will represent an actual catpic with the suffix .jpg
+// the callback function should be invoked with an array of responses once all the catpics have been collated(the order does not matter)
+// the server will respond with an error if the requested pic doesn't contain the word "cat". Therefore, if you receive an error, you must put placeholder.jpg in its place in the response array to act as a placeholder for the missing cat picture.
+// Note: You should make the request to receive the string containing .jpg rather than using a JS method!
+function fetchCatPics(arrCats, callbackFunc) {
+  request(`/pics/${arrCats}`, (err, pictures) => {
+    const finalCats = [];
+    arrCats.forEach(function (cats) {
+      finalCats.push(cats);
+    });
+    callbackFunc(null, finalCats);
+  });
+}
 
 function fetchAllCats() {}
 
